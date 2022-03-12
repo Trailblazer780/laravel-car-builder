eb1252e82f2c36d7a61257d6757b22f270cd3897
Ethan Farrell <ethan.farrell.1997@gmail.com>
2022-03-12 13:06:57 -0400

    Seeder Updated

--- database/seeders/CarPartsSeeder.php
+++ database/seeders/CarPartsSeeder.php
@@ -19,10 +19,10 @@ class CarPartsSeeder extends Seeder
      */
     public function run()
     {
-        // User::truncate();
         
         DB::table('parts')->truncate();
 
+        // ---------------------------------------------------------------------- Engine
         $turbo = Part::create([
             'part_name' => 'Turbo',
             'part_description' => 'A turbo is a type of engine modification that increases the power of an engine by increasing the flow of air into the engine. A turbo is typically a power modification, but can also be a fuel injection modification.',
@@ -31,10 +31,11 @@ class CarPartsSeeder extends Seeder
 
         $superCharger = Part::create([
             'part_name' => 'Supercharger',
-            'part_description' => 'A supercharger is a type of engine modification that increases the power of an engine by increasing the flow of air into the engine. A turbo is typically a power modification, but can also be a fuel injection modification.',
+            'part_description' => 'A supercharger is a type of engine modification that increases the power of an engine by increasing the flow of air into the engine.',
             'part_category' => 'Engine',
         ]);
 
+        // ---------------------------------------------------------------------- Brakes
         $brembo = Part::create([
             'part_name' => 'Brembo Brakes',
             'part_description' => 'A brembo is a type of brake modification that increases the breaking force',
@@ -47,12 +48,15 @@ class CarPartsSeeder extends Seeder
             'part_category' => 'Brakes',
         ]);
 
+        // ---------------------------------------------------------------------- Suspension
         $coilOvers = Part::create([
             'part_name' => 'Coil Overs',
             'part_description' => 'A coil over is a type of Suspension modification to lower the vehicle',
             'part_category' => 'Suspension',
         ]);
 
+
+        // ---------------------------------------------------------------------- Cars
         $car1 = Part::create([
             'part_name' => '2000 Subaru Legacy B4 RSK',
             'part_description' => 'Boxer 4 Cyl 2.0l Twin Turbo 280Hp',
@@ -74,6 +78,12 @@ class CarPartsSeeder extends Seeder
         $car4 = Part::create([
             'part_name' => '1999 Nissan Skyline GTR',
             'part_description' => 'Straight 6 2.6l Twin Turbo 280Hp',
+            'part_category' => 'Car',
+        ]);
+
+        $car5 = Part::create([
+            'part_name' => '2015 Mitsubishi Lancer Evolution X',
+            'part_description' => '4 Cyl 2.0l Turbo 303Hp',
             'part_category' => 'Car',
         ]);
 
